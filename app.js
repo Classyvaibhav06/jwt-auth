88998eef66a2bcf8d504f44d98692aad7a320b01
vaibhav <vaibhav7290119@gmail.com>
2026-01-12 23:51:23 +0530

    add frontend pages

--- app.js
+++ app.js
@@ -1,19 +1,30 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const jwt = require('jsonwebtoken');
-const bcrypt = require('bcrypt');
-app.use(require('cookie-parser')());
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+app.use(require("cookie-parser")());
+app.set("view engine","ejs");
+const path = require('path');
+app.set('views', path.join(__dirname, 'views'));
 
-app.get("/",(req,res)=>{
-    res.cookie("biscuit","chocolatechip");
-    res.send("Hello World");
-})
 
-app.get("/read",(req,res)=>{
+app.get("/", (req, res) => {
+  bcrypt.compare("password", "$2b$10$XZikpkTB5pZidOlpfxpgXebNJVsazcWMY10Wyo1Z2O7EPfzOuWLZ6", function(err, result) {
+    console.log(result)
+});
+
+  res.render("index");
+});
+
+// login route
+app.get("/login", (req, res) => { 
+  res.render("login");
+});
+//$2b$10$XZikpkTB5pZidOlpfxpgXebNJVsazcWMY10Wyo1Z2O7EPfzOuWLZ6
+app.get("/read", (req, res) => {
   res.send("Read");
-})
+});
 
-
-app.listen(3000,()=>{
-    console.log("Server started at port 3000");
+app.listen(3000, () => {
+  console.log("Server started at port 3000");
 });
